ee21a7091b2213654d7847a67612a89aec10183d
Uvindu  Herath <110981456+herathua@users.noreply.github.com>
2024-11-11 23:20:48 +0530

    change database tables

--- user-service/src/main/java/com/style_haven/user_service/domain/User.java
+++ user-service/src/main/java/com/style_haven/user_service/domain/User.java
@@ -3,16 +3,11 @@ package com.style_haven.user_service.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -21,33 +16,36 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @Table(name = "Users")
 @EntityListeners(AuditingEntityListener.class)
-@Getter
-@Setter
 public class User {
 
     @Id
     @Column(nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Integer userid;
 
-    @Column(columnDefinition = "longtext")
-    private String password;
+    @Column
+    private String fname;
 
-    @Column(length = 50)
-    private String firstName;
+    @Column
+    private String lname;
 
-    @Column(length = 50)
-    private String lastName;
+    @Column
+    private String state;
+
+    @Column
+    private String city;
+
+    @Column
+    private String street;
+
+    @Column
+    private String postalCode;
 
     @Column
     private String email;
 
     @Column
-    private String userName;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "address_id")
-    private Address address;
+    private String username;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -56,5 +54,93 @@ public class User {
     @LastModifiedDate
     @Column(nullable = false)
     private OffsetDateTime lastUpdated;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(final Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(final String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(final String lname) {
+        this.lname = lname;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(final String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(final String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(final String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public OffsetDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(final OffsetDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public OffsetDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(final OffsetDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 
 }
--- user-service/src/main/java/com/style_haven/user_service/model/UserDTO.java
+++ user-service/src/main/java/com/style_haven/user_service/model/UserDTO.java
@@ -1,30 +1,106 @@
 package com.style_haven.user_service.model;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 
-@Getter
-@Setter
 public class UserDTO {
 
-    private Long userId;
+    private Integer userid;
 
-    private String password;
+    @Size(max = 255)
+    private String fname;
 
-    @Size(max = 50)
-    private String firstName;
+    @Size(max = 255)
+    private String lname;
 
-    @Size(max = 50)
-    private String lastName;
+    @Size(max = 255)
+    private String state;
+
+    @Size(max = 255)
+    private String city;
+
+    @Size(max = 255)
+    private String street;
+
+    @Size(max = 255)
+    private String postalCode;
 
     @Size(max = 255)
     private String email;
 
     @Size(max = 255)
-    private String userName;
+    private String username;
 
-    private Integer address;
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(final Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(final String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(final String lname) {
+        this.lname = lname;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(final String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(final String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(final String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
 
 }
