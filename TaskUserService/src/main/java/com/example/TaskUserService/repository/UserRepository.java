e3307278ebe41b7f712ee8d0e219f44b554e980b
Godawela <godawelaus.21@uom.lk>
2024-11-14 18:12:56 +0530

    With mongo

---
+++ TaskUserService/src/main/java/com/example/TaskUserService/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.TaskUserService.repository;
+
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.TaskUserService.usermodel.User;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+    @Query("{email :?0}")
+    User findByEmail(String email);
+    
+
+}
