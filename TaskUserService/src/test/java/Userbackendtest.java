5989e168383e981685abc21bedc6b6ce120fbe6f
Chamod Weerasinghe <chamodweerasinghe8@gmail.com>
2024-11-20 16:34:29 +0530

    basic testing

---
+++ TaskUserService/src/test/java/Userbackendtest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Userbackendtest {
+    @Test
+    public void testing1() {
+        assertTrue (true);
+    }
+}
