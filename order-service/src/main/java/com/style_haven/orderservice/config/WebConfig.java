b329871dc9d452a711b8c8449bc5b14870956c29
HansaniMaleesha <maleesha0003@gmail.com>
2024-12-06 11:47:39 +0530

    Save local changes

--- order-service/src/main/java/com/style_haven/orderservice/config/WebConfig.java
+++ order-service/src/main/java/com/style_haven/orderservice/config/WebConfig.java
@@ -1,12 +1,11 @@
-package com.style_haven.paymentservice.config;
-
+package com.style_haven.orderservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CoesConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,10 +13,10 @@ public class CoesConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("http://localhost:5173") // Add allowed origins
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add allowed HTTP methods
+                        .allowedOrigins("http://localhost:5173") // Allow only the Vite dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
                         .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow credentials if needed
+                        .allowCredentials(true); // Allow credentials (if needed)
             }
         };
     }
