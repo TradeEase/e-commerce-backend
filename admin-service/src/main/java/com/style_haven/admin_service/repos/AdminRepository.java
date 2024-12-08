16bd109632a5083714559cc5fd8737339adcd6c5
Uvindu  Herath <110981456+herathua@users.noreply.github.com>
2024-12-08 07:05:34 +0530

    add docker

--- admin-service/src/main/java/com/style_haven/admin_service/repos/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.style_haven.admin_service.repos;
-
-import com.style_haven.admin_service.domain.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
--- user-service/src/main/java/com/style_haven/user_service/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.style_haven.user_service.repos;
-
-import com.style_haven.user_service.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
